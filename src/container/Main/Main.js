72beeac776e48e04235d1669db3c9d02fc65b324
siam <siammohammad@iut-dhaka.edu>
2020-06-02 23:38:21 +0600

    all primary set up is done

---
+++ src/container/Main/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <h1 className="display-4">
+          <strong>Automated Task Organizer</strong>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
