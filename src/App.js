38de00f948f0d5f033ede9382908b216affd0031
siam <siammohammad@iut-dhaka.edu>
2020-06-07 23:48:26 +0600

    added route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import Main from "../src/container/Main/Main";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/task-organizer" exact component={Main} />
+          <Route path="/" exact component={Main} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
