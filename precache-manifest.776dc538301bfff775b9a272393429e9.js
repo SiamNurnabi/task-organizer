e4479512c7a1e7592569d0328150276363ceafc1
siam <siammohammad@iut-dhaka.edu>
2020-06-07 23:49:49 +0600

    Updates

---
+++ precache-manifest.776dc538301bfff775b9a272393429e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2f153f7cfdfe74de5cd4af3ae3e638",
+    "url": "/task-organizer/index.html"
+  },
+  {
+    "revision": "4ee5e3263eca11a6088c",
+    "url": "/task-organizer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9049505a715ac8f62e16",
+    "url": "/task-organizer/static/css/main.d9053e58.chunk.css"
+  },
+  {
+    "revision": "4ee5e3263eca11a6088c",
+    "url": "/task-organizer/static/js/2.ebd9c15f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task-organizer/static/js/2.ebd9c15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9049505a715ac8f62e16",
+    "url": "/task-organizer/static/js/main.ff7b59ee.chunk.js"
+  },
+  {
+    "revision": "89bf4333fbf75f6dd891",
+    "url": "/task-organizer/static/js/runtime-main.79c3b480.js"
+  }
+]);
